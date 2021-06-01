fd6f1c5cc0b46d8625b3244a3b10fcf8ad03cf11
kiki7000 <devkiki7000@gmail.com>
2021-06-01 21:59:02 +0900

    refactor: use // instead of new RegExp()

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import {parse} from "node-html-parser";
 import * as iconv from "iconv-lite";
 
-const routereg = new RegExp(String.raw`(?<=\.)/\d+\?\d+l`)
-const prefixreg = new RegExp(String.raw`'\d+_'`)
-const orgdatareg = new RegExp(String.raw`(?<=원자료=자료\.자료)\d+`)
-const daydatareg = new RegExp(String.raw`(?<=일일자료=자료\.자료)\d+`)
-const thnamereg = new RegExp(String.raw`(?<=성명=자료\.자료)\d+`)
-const sbnamereg = new RegExp(String.raw`(?<=자료.자료)\d+(?=\[sb\])`)
+const routereg = /(?<=\.)\/\d+\?\d+l/
+const prefixreg = /'\d+_'/
+const orgdatareg = /(?<=원자료=자료\.자료)\d+/
+const daydatareg = /(?<=일일자료=자료\.자료)\d+/
+const thnamereg = /(?<=성명=자료\.자료)\d+/
+const sbnamereg = /(?<=자료.자료)\d+(?=\[sb\])/
 
-const comcigan = axios.create({baseURL: 'http://112.186.146.81:4082',})
+const comcigan: AxiosInstance = axios.create({ baseURL: 'http://112.186.146.81:4082', })
 
-comcigan.get("/st", {responseType: "arraybuffer"}).then((res) => {
+comcigan.get("/st", {responseType: "arraybuffer"}).then(res => {
     const script = parse(iconv.decode(res.data, "EUC-KR")).querySelectorAll("script")[1]!.innerHTML
     //console.log(script)
     console.log(routereg.exec(script)![0])
